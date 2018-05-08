b82f3408fa5deb790766bf1d9a5319f4093f9314
jcarrion <jcarrion@salesforce.com>
2018-05-08 20:01:16 +0200

    make it better

---
+++ register.sh
@@ -0,0 +1,3 @@
+sfdx force:auth:jwt:grant --clientid 3MVG9X0_oZyBSzHrtbrbfMcbIYRG2EJYKx.kHJqYn5fr_CJypNQvV0UaNy5ALJEqbHm8fuglPg6J0VxFdsCKa --jwtkeyfile /app/repaudit/server.key --username jcarrion@salesforce.com.repsol.repaudit --instanceurl https://test.salesforce.com --setalias repaudit
+
+sfdx force:org:list
