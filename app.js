272e155768993315b4c8f268a620a3b04b4d9406
jcarrion <jcarrion@salesforce.com>
2018-05-08 21:25:44 +0200

    make it better

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ console.log('hola mundo  ');
 
 nrc.run('sfdx force:auth:jwt:grant --clientid 3MVG9X0_oZyBSzHrtbrbfMcbIYRG2EJYKx.kHJqYn5fr_CJypNQvV0UaNy5ALJEqbHm8fuglPg6J0VxFdsCKa --jwtkeyfile /app/repaudit/server.key --username jcarrion@salesforce.com.repsol.repaudit --instanceurl https://test.salesforce.com --setalias repaudit');
 
-nrc.run('sfdx force:org:list --verbose --json');
+nrc.run();
 
 var dataCallback = function(data) {
   console.log('data es  ' + data);
 };
 
-nrc.run('ls', { onData: dataCallback });
+nrc.run('sfdx force:org:list --verbose --json', { onData: dataCallback });
 
 console.log('hola mundo2  ');
