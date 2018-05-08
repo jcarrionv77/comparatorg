884ac430aa4e7bec20045c579c8b1d872f4a737f
jcarrion <jcarrion@salesforce.com>
2018-05-09 00:28:25 +0200

    make it better

--- app.js
+++ app.js
@@ -4,27 +4,32 @@ var nrc = require('node-run-cmd');
 
 console.log('hola mundo  ');
 
+try {
+
+	nrc.run('sfdx force:auth:jwt:grant --clientid 3MVG9X0_oZyBSzHrtbrbfMcbIYRG2EJYKx.kHJqYn5fr_CJypNQvV0UaNy5ALJEqbHm8fuglPg6J0VxFdsCKa --jwtkeyfile /app/repaudit/server.key --username jcarrion@salesforce.com.repsol.repaudit --instanceurl https://test.salesforce.com --setalias repaudit', { onDone: doneLogin, onError:errorCallback});
+
+	var doneLogin = function(code) {
+	  console.log('code  ' + code);
+	};
 
 
-nrc.run('sfdx force:auth:jwt:grant --clientid 3MVG9X0_oZyBSzHrtbrbfMcbIYRG2EJYKx.kHJqYn5fr_CJypNQvV0UaNy5ALJEqbHm8fuglPg6J0VxFdsCKa --jwtkeyfile /app/repaudit/server.key --username jcarrion@salesforce.com.repsol.repaudit --instanceurl https://test.salesforce.com --setalias repaudit', { onDone: doneLogin, onError:errorCallback});
+	var errorCallback = function(err) {
+	  console.log('err  ' + err);
+	};
 
-var doneLogin = function(code) {
-  console.log('code  ' + code);
-};
+	/*
 
+	var dataCallback = function(data) {
+	  var miJson = data;
+	  console.log('dataCallback  ');
+	  var jsonContent = JSON.parse(miJson);
+	  console.log('miJson  ' + miJson);
+	};
+	nrc.run('sfdx force:org:list --verbose --json', { onData: dataCallback , onError:errorCallback});*/
 
-var errorCallback = function(err) {
-  console.log('err  ' + err);
-};
-
-/*
-
-var dataCallback = function(data) {
-  var miJson = data;
-  console.log('dataCallback  ');
-  var jsonContent = JSON.parse(miJson);
-  console.log('miJson  ' + miJson);
-};
-nrc.run('sfdx force:org:list --verbose --json', { onData: dataCallback , onError:errorCallback});*/
-
-console.log('hola mundo2  ');
+	console.log('hola mundo2  ');
+}
+catch (err) {
+		
+  		console.error(err);
+	}
