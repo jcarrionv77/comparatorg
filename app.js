daa5056caeae1fb370c0a1edc655502055730af6
jcarrion <jcarrion@salesforce.com>
2018-05-09 00:25:23 +0200

    make it better

--- app.js
+++ app.js
@@ -6,16 +6,18 @@ console.log('hola mundo  ');
 
 
 
-nrc.run('sfdx force:auth:jwt:grant --clientid 3MVG9X0_oZyBSzHrtbrbfMcbIYRG2EJYKx.kHJqYn5fr_CJypNQvV0UaNy5ALJEqbHm8fuglPg6J0VxFdsCKa --jwtkeyfile /app/repaudit/server.key --username jcarrion@salesforce.com.repsol.repaudit --instanceurl https://test.salesforce.com --setalias repaudit', { onDone: doneLogin});
+nrc.run('sfdx force:auth:jwt:grant --clientid 3MVG9X0_oZyBSzHrtbrbfMcbIYRG2EJYKx.kHJqYn5fr_CJypNQvV0UaNy5ALJEqbHm8fuglPg6J0VxFdsCKa --jwtkeyfile /app/repaudit/server.key --username jcarrion@salesforce.com.repsol.repaudit --instanceurl https://test.salesforce.com --setalias repaudit', { onDone: doneLogin, onError:errorCallback});
 
 var doneLogin = function(code) {
   console.log('code  ' + code);
 };
 
-/*
+
 var errorCallback = function(err) {
   console.log('err  ' + err);
 };
+
+/*
 
 var dataCallback = function(data) {
   var miJson = data;
