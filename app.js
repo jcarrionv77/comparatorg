1da62322449a7b18e95d1b3e6c63fdc2951cacd1
jcarrion <jcarrion@salesforce.com>
2018-05-09 00:23:48 +0200

    make it better

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ console.log('hola mundo  ');
 
 
 
-nrc.run('sfdx force:auth:jwt:grant --clientid 3MVG9X0_oZyBSzHrtbrbfMcbIYRG2EJYKx.kHJqYn5fr_CJypNQvV0UaNy5ALJEqbHm8fuglPg6J0VxFdsCKa --jwtkeyfile /app/repaudit/server.key --username jcarrion@salesforce.com.repsol.repaudit --instanceurl https://test.salesforce.com --setalias repaudit');
+nrc.run('sfdx force:auth:jwt:grant --clientid 3MVG9X0_oZyBSzHrtbrbfMcbIYRG2EJYKx.kHJqYn5fr_CJypNQvV0UaNy5ALJEqbHm8fuglPg6J0VxFdsCKa --jwtkeyfile /app/repaudit/server.key --username jcarrion@salesforce.com.repsol.repaudit --instanceurl https://test.salesforce.com --setalias repaudit', { onDone: doneLogin});
 
+var doneLogin = function(code) {
+  console.log('code  ' + code);
+};
 
+/*
 var errorCallback = function(err) {
   console.log('err  ' + err);
 };
@@ -19,6 +23,6 @@ var dataCallback = function(data) {
   var jsonContent = JSON.parse(miJson);
   console.log('miJson  ' + miJson);
 };
-nrc.run('sfdx force:org:list --verbose --json', { onData: dataCallback , onError:errorCallback});
+nrc.run('sfdx force:org:list --verbose --json', { onData: dataCallback , onError:errorCallback});*/
 
 console.log('hola mundo2  ');
