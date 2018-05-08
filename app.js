e2a5ca131346e122bb727a330c1d4a298bd19038
jcarrion <jcarrion@salesforce.com>
2018-05-09 00:44:39 +0200

    make it better

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ var nrc = require('node-run-cmd');
 
 try {
 
-console.log('hola mundo  ');/*
+console.log('hola mundo  ');
 	nrc.run('sfdx force:auth:jwt:grant --clientid 3MVG9X0_oZyBSzHrtbrbfMcbIYRG2EJYKx.kHJqYn5fr_CJypNQvV0UaNy5ALJEqbHm8fuglPg6J0VxFdsCKa --jwtkeyfile /app/repaudit/server.key --username jcarrion@salesforce.com.repsol.repaudit --instanceurl https://test.salesforce.com --setalias repaudit');
 
+/*
 	var doneLogin = function(code) {
 	  console.log('code  ' + code);
 	};
