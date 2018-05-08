50e766e9635dbfd8ba1e11592276a983674deb72
jcarrion <jcarrion@salesforce.com>
2018-05-08 22:10:44 +0200

    make it better

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ var nrc = require('node-run-cmd');
 
 console.log('hola mundo  ');
 
-
+nrc.run('mkdir tmp');
 
 nrc.run('sfdx force:auth:jwt:grant --clientid 3MVG9X0_oZyBSzHrtbrbfMcbIYRG2EJYKx.kHJqYn5fr_CJypNQvV0UaNy5ALJEqbHm8fuglPg6J0VxFdsCKa --jwtkeyfile /app/repaudit/server.key --username jcarrion@salesforce.com.repsol.repaudit --instanceurl https://test.salesforce.com --setalias repaudit');
 
-nrc.run();
+nrc.run('sfdx force:org:list --verbose --json > tmp/MyOrgList.json');
 
-var dataCallback = function(data) {
-  console.log('data es  ' + data);
-};
+var contents = fs.readFileSync("tmp/MyOrgList.json");
+var jsonContent = JSON.parse(contents);
 
-nrc.run('sfdx force:org:list --verbose --json', { onData: dataCallback });
+console.log('contents  ' + contents);
+
 
 console.log('hola mundo2  ');
