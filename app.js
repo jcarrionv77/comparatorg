9a26b15b2135c3c6c174442ff48515e3c8e6ec84
jcarrion <jcarrion@salesforce.com>
2018-05-09 15:39:17 +0200

    Demo

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 var fs = require("fs");
 var nrc = require('node-run-cmd');
 
+const { exec } = require('child_process');
+
 
 
 
 try {
 
 console.log('hola mundo  ');
-	nrc.run('sfdx force:auth:jwt:grant --clientid 3MVG9X0_oZyBSzHrtbrbfMcbIYRG2EJYKx.kHJqYn5fr_CJypNQvV0UaNy5ALJEqbHm8fuglPg6J0VxFdsCKa --jwtkeyfile /app/repaudit/server.key --username jcarrion@salesforce.com.repsol.repaudit --instanceurl https://test.salesforce.com --setalias repaudit');
+
+var commandSFDX = 'sfdx force:auth:jwt:grant --clientid 3MVG9X0_oZyBSzHrtbrbfMcbIYRG2EJYKx.kHJqYn5fr_CJypNQvV0UaNy5ALJEqbHm8fuglPg6J0VxFdsCKa --jwtkeyfile ./repaudit/server.key --username jcarrion@salesforce.com.repsol.repaudit --instanceurl https://test.salesforce.com --setalias repaudit';
+
+var command = 'find . -type f | wc -l';
+
+exec(commandSFDX, (err, stdout, stderr) => {
+  if (err) {
+    console.error(`exec error: ${err}`);
+    return;
+  }
+
+  console.log(`exec ok  ${stdout}`);
+});
+
+
+
+	//nrc.run('sfdx force:auth:jwt:grant --clientid 3MVG9X0_oZyBSzHrtbrbfMcbIYRG2EJYKx.kHJqYn5fr_CJypNQvV0UaNy5ALJEqbHm8fuglPg6J0VxFdsCKa --jwtkeyfile /app/repaudit/server.key --username jcarrion@salesforce.com.repsol.repaudit --instanceurl https://test.salesforce.com --setalias repaudit');
 
 /*
 	var doneLogin = function(code) {
