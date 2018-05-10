384df817e330f5a027185663a90c83eb6f7a4987
jcarrion <jcarrion@salesforce.com>
2018-05-10 17:42:14 +0200

    Demo

--- app.js
+++ app.js
@@ -1,87 +1,100 @@
 var fs = require("fs");
-var nrc = require('node-run-cmd');
+var pg = require('pg');
+
+var dbCli = null;
+var instanciasArray = new Array();
 
 const { exec } = require('child_process');
 
 
-
-
-try {
-
-console.log('hola mundo  ');
-
-var serverKey = process.env.SERVER_KEY;
-
-var fs = require('fs');
-fs.writeFileSync("tmp/server.key", serverKey); 
-
-var commandSFDXLogin = 'sfdx force:auth:jwt:grant --clientid 3MVG9X0_oZyBSzHrtbrbfMcbIYRG2EJYKx.kHJqYn5fr_CJypNQvV0UaNy5ALJEqbHm8fuglPg6J0VxFdsCKa --jwtkeyfile ./tmp/server.key --username jcarrion@salesforce.com.repsol.repaudit --instanceurl https://test.salesforce.com --setalias repaudit';
-var commandSFDXList = 'sfdx force:org:list --verbose --json > tmp/MyOrgList.json';
-
-
-var command = 'find . -type f | wc -l';
-
-exec(commandSFDXLogin, (err, stdout, stderr) => {
-  if (err) {
-    console.error(`exec error: ${err}`);
-    return;
-  }
-
-  console.log(`exec ok  ${stdout}`);
-  console.log('FIN exec login');
-
-
-	exec(commandSFDXList, (err, stdout, stderr) => {
-	  if (err) {
-	    console.error(`exec error: ${err}`);
-	    return;
-	  }
-
-	  console.log(`exec ok  ${stdout}`);
-	  console.log('FIN exec login');
-
-		var contents = fs.readFileSync("tmp/MyOrgList.json");
-		var jsonContent = JSON.parse(contents);
-
-		console.log('contents ' + contents);
-		console.log('END');
-
-	});
-
-
-
-
-});
-
-
-
-
-
-	//nrc.run('sfdx force:auth:jwt:grant --clientid 3MVG9X0_oZyBSzHrtbrbfMcbIYRG2EJYKx.kHJqYn5fr_CJypNQvV0UaNy5ALJEqbHm8fuglPg6J0VxFdsCKa --jwtkeyfile /app/repaudit/server.key --username jcarrion@salesforce.com.repsol.repaudit --instanceurl https://test.salesforce.com --setalias repaudit');
-
-/*
-	var doneLogin = function(code) {
-	  console.log('code  ' + code);
-	};
-
-
-	var errorCallback = function(err) {
-	  console.log('err  ' + err);
-	};
-
-	
-
-	var dataCallback = function(data) {
-	  var miJson = data;
-	  console.log('dataCallback  ');
-	  var jsonContent = JSON.parse(miJson);
-	  console.log('miJson  ' + miJson);
-	};
-	nrc.run('sfdx force:org:list --verbose --json', { onData: dataCallback , onError:errorCallback});*/
-
-	console.log('hola mundo2  ');
-}
-catch (err) {
+function start()
+{
+	pg.defaults.ssl = true;
+	pg.connect(process.env.DATABASE_URL, function(err, client) {
+		if (err) 
+			throw err;
 		
-  		console.error(err);
+		console.log('Connected to postgres!');
+		dbCli = client;
+
+		dbCli.query(
+			'SELECT nombre, secreto, usuario FROM instancias', 
+			function(err, result) {
+			if (err) {
+				console.log(err);
+			} 
+			else if  (result != null && result.rows.length>0)
+			{
+				for (int i=0; i<result.rows.length; i++)
+				{
+					var instancia = {};
+					instancia.nombre = result.rows[i].nombre;
+					instancia.secreto = result.rows[i].secreto;
+					instancia.usuario = result.rows[i].usuario;
+
+					instanciasArray.push(instancia);
+				}
+				whenConnected();
+			}
+		}); 
+
+    });
+}
+
+
+function whenConnected()
+{
+    console.log('whenConnected!');
+	try {
+
+		console.log('hola mundo  ');
+
+		console.log('instanciasArray  ' + instanciasArray);
+
+		var serverKey = process.env.SERVER_KEY;
+
+		var fs = require('fs');
+		fs.writeFileSync("tmp/server.key", serverKey); 
+
+		var commandSFDXLogin = 'sfdx force:auth:jwt:grant --clientid 3MVG9X0_oZyBSzHrtbrbfMcbIYRG2EJYKx.kHJqYn5fr_CJypNQvV0UaNy5ALJEqbHm8fuglPg6J0VxFdsCKa --jwtkeyfile ./tmp/server.key --username jcarrion@salesforce.com.repsol.repaudit --instanceurl https://test.salesforce.com --setalias repaudit';
+		var commandSFDXList = 'sfdx force:org:list --verbose --json > tmp/MyOrgList.json';
+
+
+		var command = 'find . -type f | wc -l';
+
+		exec(commandSFDXLogin, (err, stdout, stderr) => {
+			if (err) {
+				console.error(`exec error: ${err}`);
+				return;
+			}
+
+			console.log(`exec ok  ${stdout}`);
+			console.log('FIN exec login');
+
+
+			exec(commandSFDXList, (err, stdout, stderr) => {
+				if (err) {
+					console.error(`exec error: ${err}`);
+					return;
+				}
+
+				console.log(`exec ok  ${stdout}`);
+				console.log('FIN exec login');
+
+				var contents = fs.readFileSync("tmp/MyOrgList.json");
+				var jsonContent = JSON.parse(contents);
+
+				console.log('contents ' + contents);
+				console.log('END');
+
+			});
+		});
+	console.log('hola mundo2  ');
 	}
+	catch (err) {
+		console.error(err);
+	}
+}
+
+start();
+
