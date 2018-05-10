a18640da69b508413655ea22f022f7b252267613
jcarrion <jcarrion@salesforce.com>
2018-05-10 11:52:15 +0200

    Demo

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ try {
 
 console.log('hola mundo  ');
 
-var commandSFDXLogin = 'sfdx force:auth:jwt:grant --clientid 3MVG9X0_oZyBSzHrtbrbfMcbIYRG2EJYKx.kHJqYn5fr_CJypNQvV0UaNy5ALJEqbHm8fuglPg6J0VxFdsCKa --jwtkeyfile ./repaudit/server.key --username jcarrion@salesforce.com.repsol.repaudit --instanceurl https://test.salesforce.com --setalias repaudit';
+var serverKey = process.env.SERVER_KEY;
+
+var fs = require('fs');
+fs.writeFileSync("/tmp/server.key", serverKey); 
+
+var commandSFDXLogin = 'sfdx force:auth:jwt:grant --clientid 3MVG9X0_oZyBSzHrtbrbfMcbIYRG2EJYKx.kHJqYn5fr_CJypNQvV0UaNy5ALJEqbHm8fuglPg6J0VxFdsCKa --jwtkeyfile ./tmp/server.key --username jcarrion@salesforce.com.repsol.repaudit --instanceurl https://test.salesforce.com --setalias repaudit';
 var commandSFDXList = 'sfdx force:org:list --verbose --json > tmp/MyOrgList.json';
 
 
