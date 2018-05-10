9eedb202b201f99b119a6eef991655efe698b440
jcarrion <jcarrion@salesforce.com>
2018-05-10 18:11:49 +0200

    Demo

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function start()
 		dbCli = client;
 
 		dbCli.query(
-			'SELECT nombre, secreto, usuario FROM instancias', 
+			'SELECT nombre, secreto, usuario, url FROM instancias', 
 			function(err, result) {
 			if (err) {
 				console.log(err);
@@ -31,8 +31,7 @@ function start()
 					instancia.nombre = result.rows[i].nombre;
 					instancia.secreto = result.rows[i].secreto;
 					instancia.usuario = result.rows[i].usuario;
-
-					console.log('instancia  ' + instancia.stringify());
+					instancia.url = result.rows[i].url;
 
 					instanciasArray.push(instancia);
 				}
@@ -58,7 +57,18 @@ function whenConnected()
 		var fs = require('fs');
 		fs.writeFileSync("tmp/server.key", serverKey); 
 
-		var commandSFDXLogin = 'sfdx force:auth:jwt:grant --clientid 3MVG9X0_oZyBSzHrtbrbfMcbIYRG2EJYKx.kHJqYn5fr_CJypNQvV0UaNy5ALJEqbHm8fuglPg6J0VxFdsCKa --jwtkeyfile ./tmp/server.key --username jcarrion@salesforce.com.repsol.repaudit --instanceurl https://test.salesforce.com --setalias repaudit';
+		var commandSFDXLogin = '';
+
+		
+		for (var i=0; i<instanciasArray.length; i++)
+		{
+			commandSFDXLogin = 'sfdx force:auth:jwt:grant --clientid ' + instanciasArray[i].secreto +' --jwtkeyfile ./tmp/server.key --username ' + instanciasArray[i].usuario + ' --instanceurl ' + instanciasArray[i].url+ ' --setalias '+ instanciasArray[i].nombre;
+		}
+
+		//var commandSFDXLogin = 'sfdx force:auth:jwt:grant --clientid 3MVG9X0_oZyBSzHrtbrbfMcbIYRG2EJYKx.kHJqYn5fr_CJypNQvV0UaNy5ALJEqbHm8fuglPg6J0VxFdsCKa --jwtkeyfile ./tmp/server.key --username jcarrion@salesforce.com.repsol.repaudit --instanceurl https://test.salesforce.com --setalias repaudit';
+		
+		console.log('commandSFDXLogin  ' + commandSFDXLogin);
+
 		var commandSFDXList = 'sfdx force:org:list --verbose --json > tmp/MyOrgList.json';
 
 
