bb8f0e2bac413809fd84927552a8d7baf1240fd4
jcarrion <jcarrion@salesforce.com>
2018-05-11 18:16:32 +0200

    Demo

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ var app = express();
 
 app.use(bodyParser.json());
 
+app.get('/', function(req, res) {
 
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
 
 
 app.listen(port, function() {
