4a5fcd61458a76f0b51cddba6d9ed7bb6b565a7a
jcarrion <jcarrion@salesforce.com>
2018-05-11 16:30:07 +0200

    Demo

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ function ConsultaInstancias()
 				}
 				descargaFicheros(0);
 				console.log('Fin descarga !!!!!!!!');
-				stopWorker();
+				read();
 			}
 		}); 
 
@@ -193,6 +193,133 @@ function describeObject(instancia , iteracion)
 	*/
 
 }
+
+funtion read(){
+	for (var i=0; i<objetosArray.length; i++)
+	{
+		readFiles(objetosArray[i].apiname);
+	}
+	console.log('fin read  **********' );
+
+}
+
+
+function readFiles(obj){
+
+	var directorioObj = 'tmp/' + obj;
+
+	console.log('readFiles en  ' + directorioObj);
+	var files = fs.readdirSync(directorioObj);
+	var fieldsArray = new Array();
+	var orgsArray = new Array();
+	var camposArray = new Array(); 
+
+	files.forEach(file => {
+		try{
+
+			//console.log('file ' + file);
+			if(path.extname(file) == '.json' && file != 'DevHub.json'  &&  file != 'repaudit.json')
+			{
+				//console.log('file ' + file);
+
+				var org = { name: '', fields: []};
+				org.name = file;
+				var nameOrg = file.substring(0,file.length-5);
+				var MyFile = fs.readFileSync(directorioObj+'/'+file);
+
+				if(MyFile.length>0)
+				{
+
+					var jsonContent = JSON.parse(MyFile);
+					var fields = new Array();
+
+					for (var i =0; i<jsonContent.result.fields.length; i++)
+					{
+						var field = {};
+						var campo = new Object()
+						field.name = jsonContent.result.fields[i].name;
+
+						
+						campo.Name= nameOrg + jsonContent.result.fields[i].name;		
+						campo.JCV_fld_sandbox__c= nameOrg;
+						campo.JCV_fld_field__c= jsonContent.result.fields[i].name;
+						campo.JCV_fld_object__c = miObjeto;
+						camposArray.push(campo);
+
+						fields.push(field);
+						org.fields.push(jsonContent.result.fields[i].name);
+						fieldsArray.push(jsonContent.result.fields[i].name);
+					}
+
+					orgsArray.push(org);
+				}
+			}
+		}
+		catch (err) {
+			console.error('Error en files.forEach');
+  			console.error(err);
+		}
+	});
+
+
+	var camposJSON = JSON.parse(JSON.stringify(camposArray));
+
+	unique(fieldsArray);
+	var sortFieldsArray = fieldsArray.sort();
+
+	var fieldResult = new Array();
+	
+	var map = new HashMap();
+
+	for(var i=0; i< sortFieldsArray.length; i++){
+		map.set(sortFieldsArray[i], i);
+	}
+
+	for (var k=0; k<orgsArray.length;k++){
+	
+		var camposOrg = new Array(sortFieldsArray.length);  
+
+		for(j=0;j<orgsArray[k].fields.length;j++)
+		{
+			camposOrg[map.get(orgsArray[k].fields[j])] = 'si';
+		}
+		fieldResult.push(camposOrg);
+	}
+	
+	htmlTanspuesto = '<table border=&quot;1&quot;>';
+	htmlTanspuesto = htmlTanspuesto + '<tr><th>Fields</th>';
+
+	for (var k=0; k<orgsArray.length;k++){
+
+		var orgName = orgsArray[k].name;
+		htmlTanspuesto = htmlTanspuesto + '<th>' + orgName.substring(0,orgName.length-5) + '</th>';
+	}
+	html = html + '</tr>';
+	for(var i=0; i< sortFieldsArray.length; i++){
+
+		if(sortFieldsArray[i].includes("__c"))
+		{	
+
+			htmlTanspuesto = htmlTanspuesto + '<tr><td>' + sortFieldsArray[i] + '</td>';
+			for (var k=0; k<orgsArray.length;k++){
+
+				if(fieldResult[k][i] == 'undefined' || fieldResult[k][i] == '' || fieldResult[k][i] == null)
+					htmlTanspuesto = htmlTanspuesto + '<td>' + '' + '</td>';
+				else
+					htmlTanspuesto = htmlTanspuesto + '<td>' + fieldResult[k][i] + '</td>';
+			}
+			htmlTanspuesto = htmlTanspuesto + '</tr>';
+		}
+		
+	}
+	htmlTanspuesto = htmlTanspuesto + '</table>';
+
+	console.log(htmlTanspuesto);
+
+
+}
+
+
 
 function stopWorker()
 {
