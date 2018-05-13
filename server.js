4bc65bb809535c60476824521fd2759611289dfa
jcarrion <jcarrion@salesforce.com>
2018-05-13 18:02:43 +0200

    Demo

--- server.js
+++ server.js
@@ -10,18 +10,6 @@ var port = process.env.PORT || 8080;
 
 var objetosArray = new Array();
 
-function consultaObjetos()
-{
-	pg.defaults.ssl = true;
-	
-	dbCli = pg.connect(process.env.DATABASE_URL); 
-	
-	var a  = dbCli.query('SELECT nombre, apiname FROM objetos');
-	
-	console.log('a es ' + a);
-	
-
-}
 
 
 app.use(bodyParser.json());
@@ -39,15 +27,61 @@ app.get('/', function(req, res) {
 
 	console.log('hola mundo');
 
-	consultaObjetos();
+pg.defaults.ssl = true;
+	pg.connect(process.env.DATABASE_URL, function(err, client) {
+		if (err) 
+			throw err;
+		
+		console.log('Connected to postgres!');
+		dbCli = client;
+
+		dbCli.query(
+			'SELECT nombre, apiname FROM objetos', 
+			function(err, result) {
+			if (err) {
+				console.log(err);
+			} 
+			else if  (result != null && result.rows.length>0)
+			{
+				for (var i=0; i<result.rows.length; i++)
+				{
+					var objeto = {};
+					objeto.nombre = result.rows[i].nombre;
+					objeto.apiname = result.rows[i].apiname;
+
+					objetosArray.push(objeto);
+
+					var directorio = 'tmp/'+objeto.nombre
+					execSync('mkdir ' + directorio);
+				}
+
+
+				console.log('fin consultaObjetos');
+
+			res.render('index',{users : [
+			            { name: 'John' },
+			            { name: 'Mike' },
+			            { name: 'Samantha' }
+			  	]});
+
+
+				
+			}
+		}); 
+
+    });
+
+
+
 
 
     // ejs render automatically looks in the views folder
-    res.render('index',{users : [
+    /*res.render('index',{users : [
             { name: 'John' },
             { name: 'Mike' },
             { name: 'Samantha' }
-  	]});
+  	]});*/
+
 });
 
 
