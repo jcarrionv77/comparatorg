6fa57d61d4d8d6af2eb3f8bcc9a1cb04cc539ea6
jcarrion <jcarrion@salesforce.com>
2018-05-13 17:55:03 +0200

    Demo

--- server.js
+++ server.js
@@ -5,6 +5,23 @@ var bodyParser = require('body-parser');
 var app = express();
 var port = process.env.PORT || 8080;
 
+var objetosArray = new Array();
+
+function consultaObjetos()
+{
+	pg.defaults.ssl = true;
+	pg.connect(process.env.DATABASE_URL, function(err, client) {
+		if (err) 
+			throw err;
+		
+		console.log('Connected to postgres!');
+		dbCli = client;
+
+		var a  = dbCli.query('SELECT nombre, apiname FROM objetos');
+		console.log('a es ' + a);
+
+}
+
 
 app.use(bodyParser.json());
 var routes = require('./routes/index');
@@ -18,6 +35,11 @@ app.use('/routes', routes);
 
 
 app.get('/', function(req, res) {
+
+	console.log('hola mundo');
+
+	consultaObjetos();
+
 
     // ejs render automatically looks in the views folder
     res.render('index',{users : [
