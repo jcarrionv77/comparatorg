0acc426aa252238249813f4b3665989bcb3f5ceb
jcarrion <jcarrion@salesforce.com>
2018-07-20 16:23:38 +0200

    Demo

--- app.js
+++ app.js
@@ -619,7 +619,7 @@ function consultaLicencias(){
 	execSync('mkdir ' + directorio);
 
 	var fileName = './tmp/licencias/asignacionPS.json'
-	var sQuery = 'SELECT Assignee.name,  Assignee.LastLoginDate, Assignee.Profile.Name, Assignee.Profile.Id, Assignee.Profile.UserLicense.name,Id,  PermissionSet.name FROM PermissionSetAssignment  where PermissionSet.IsOwnedByProfile=false and  Assignee.IsActive = true and Assignee.Profile.UserLicense.name= \'Salesforce\' order by Assignee.name';
+	var sQuery = 'SELECT Assignee.name,  Assignee.LastLoginDate, Assignee.Profile.Name, Assignee.Profile.Id, Assignee.Profile.UserLicense.name,Id,  PermissionSet.name FROM PermissionSetAssignment  where Assignee.IsActive = true and Assignee.Profile.UserLicense.name= \'Salesforce\' order by Assignee.name';
 	var sProduccion = 'produccion';
 	var commandSFDXDescribe	= 'sfdx force:data:soql:query -q "' + sQuery  +  '" ' + '-u ' + sProduccion +' --json > ' + fileName;
 
@@ -724,8 +724,8 @@ function consultaLicencias(){
 
 	for (var i =0; i<jsonContent.result.records.length; i++)
 	{
-		if(jsonContent.result.records[i].Assignee.Name != 'JOSE SANCHEZ-QUINTANAR SANCHEZ-ALARCOS')
-		{
+		//if(jsonContent.result.records[i].Assignee.Name != 'JOSE SANCHEZ-QUINTANAR SANCHEZ-ALARCOS')
+		//{
 			if(i == 0 || jsonContent.result.records[i].Assignee.Name != jsonContent.result.records[i-1].Assignee.Name)
 			{
 				if (i>0){
@@ -753,7 +753,20 @@ function consultaLicencias(){
 
 					for (var j=0; j<miPS.arrayApps.length; j++)
 					{
-						if(miPS.arrayApps[j] != 'Chatter Repsol'){
+						if(miPS.arrayApps[j] != 'Chatter Repsol' && miPS.arrayApps[j] != 'App Launcher' 
+								 && miPS.arrayApps[j] != 'All Tabs' 
+								 && miPS.arrayApps[j] != 'Sales Console'
+								 && miPS.arrayApps[j] != 'Sales'
+								 && miPS.arrayApps[j] != 'Content'
+								 && miPS.arrayApps[j] != 'Sample Console'
+								 && miPS.arrayApps[j] != 'Platform'
+								 && miPS.arrayApps[j] != 'Service Console'
+								 && miPS.arrayApps[j] != 'Service'
+								 && miPS.arrayApps[j] != 'Marketing'
+								 && miPS.arrayApps[j] != 'Analytics Studio'
+								 && miPS.arrayApps[j] != 'Sales'
+								 && miPS.arrayApps[j] != 'Lightning Usage App') 
+						{
 							userAppsArray.push(miPS.arrayApps[j]);
 							listaAppProcesada.push(miPS.arrayApps[j]);
 							//console.log('app : ' + miPS.arrayApps[j]);
@@ -776,15 +789,28 @@ function consultaLicencias(){
 
 					for (var j=0; j<miPS.arrayApps.length; j++)
 					{
-						if(miPS.arrayApps[j] != 'Chatter Repsol'){
+						if(miPS.arrayApps[j] != 'Chatter Repsol' && miPS.arrayApps[j] != 'App Launcher' 
+								 && miPS.arrayApps[j] != 'All Tabs' 
+								 && miPS.arrayApps[j] != 'Sales Console'
+								 && miPS.arrayApps[j] != 'Sales'
+								 && miPS.arrayApps[j] != 'Content'
+								 && miPS.arrayApps[j] != 'Sample Console'
+								 && miPS.arrayApps[j] != 'Platform'
+								 && miPS.arrayApps[j] != 'Service Console'
+								 && miPS.arrayApps[j] != 'Service'
+								 && miPS.arrayApps[j] != 'Marketing'
+								 && miPS.arrayApps[j] != 'Analytics Studio'
+								 && miPS.arrayApps[j] != 'Sales'
+								 && miPS.arrayApps[j] != 'Lightning Usage App') 	
+						{
 							userAppsArray.push(miPS.arrayApps[j]);
 							listaAppProcesada.push(miPS.arrayApps[j]);
-							//console.log('app : ' + miPS.arrayApps[j]);
+							console.log('app : ' + miPS.arrayApps[j]);
 						}
 					}
 				}
 			}
-		}
+		//}
 	}
 
 	objUser.userAppsArray = userAppsArray;
